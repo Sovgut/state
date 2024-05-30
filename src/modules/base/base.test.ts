676cfa22ec162976aa46dcceb5d425b06bf61948
Sergey Sovgut <sergey.sovgut@outlook.com>
2024-05-31 02:56:23 +0300

    fixed build

--- src/modules/base/base.test.ts
+++ src/modules/base/base.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { afterEach, describe, expect, it, vi } from "vitest";
 import { BaseState } from "./base";
 import { UnsupportedException } from "@/errors/unsupported/unsupported";
 
