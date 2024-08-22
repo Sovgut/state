c88c97c4bffacdc63ae2b8f61b9d328978941c1e
Serhii Sovhut <sergey.sovgut@outlook.com>
2024-08-22 23:07:45 +0300

    fixed serializing the values

--- src/utils/serialize.ts
+++ src/utils/serialize.ts
@@ -1,38 +1,56 @@
 import { IStrategyOptions } from "@/types";
 
 export function serialize<Fallback = unknown>(value: string | undefined | null, options?: IStrategyOptions<Fallback>): Fallback {
-  const fallback = (options?.fallback ?? value ?? undefined) as Fallback;
+  // Check empty values
+  if (typeof value === "undefined") return options?.fallback as Fallback;
+  if (value === undefined) return options?.fallback as Fallback;
+  if (value === null) return options?.fallback as Fallback;
 
-  if (typeof value === "undefined" || value === undefined) return fallback;
-  if (value === null) {
-    return fallback;
-  }
-
+  // Check falsy-true values
   if (!options?.allowAnyString) {
-    if (value === 'undefined') return fallback;
-    if (value === 'null') return fallback;
-    if (value === 'NaN') return fallback;
+    if (value === 'undefined') return options?.fallback as Fallback;
+    if (value === 'null') return options?.fallback as Fallback;
+    if (value === 'NaN') return options?.fallback as Fallback;
   }
 
-  const type = options?.cast ?? typeof options?.fallback;
-  switch (type) {
-    case "bigint": {
-      return BigInt(value) as Fallback;
+  // Get target type parsing
+  const type = options?.cast ?? typeof options?.fallback ?? undefined;
+
+  // Parse value
+  try {
+    switch (type) {
+      case "bigint": {
+        return BigInt(value) as Fallback;
+      }
+      case "boolean": {
+        const parsed = value.toLowerCase()
+
+        if (parsed !== "true" && parsed !== "false") {
+          return options?.fallback as Fallback;
+        }
+
+        return (parsed === "true") as Fallback;
+      }
+      case "number": {
+        const parsed = Number(value);
+
+        if (Number.isNaN(parsed)) {
+          return options?.fallback as Fallback;
+        }
+
+        return parsed as Fallback;
+      }
+      case "object": {
+        return JSON.parse(value) as Fallback;
+      }
+      case "string": {
+        return value as Fallback;
+      }
+      default: {
+        return value as Fallback;
+      }
     }
-    case "boolean": {
-      return (value.toLowerCase() === ("true" as unknown)) as Fallback;
-    }
-    case "number": {
-      return Number(value) as Fallback;
-    }
-    case "object": {
-      return JSON.parse(value) as Fallback;
-    }
-    case "string": {
-      return value as Fallback;
-    }
-    default: {
-      return value as Fallback;
-    }
+  } catch (error) {
+    return options?.fallback as Fallback;
   }
 }
